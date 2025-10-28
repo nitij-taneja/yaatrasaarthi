428f62f2c29eb341f7cb009c4bf606f6b1d783ee
Nitij Taneja <124418733+nitij-taneja@users.noreply.github.com>
2025-10-28 22:10:31 +0530

    Update main.py

--- yatra_saarthi_django/main.py
+++ yatra_saarthi_django/main.py
@@ -10,7 +10,7 @@ from src.routes.user import user_bp
 from src.routes.chatbot import chatbot_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
-app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
+app.config['SECRET_KEY'] = 'aT'
 
 # Enable CORS for all routes
 CORS(app)
